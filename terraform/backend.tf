23a48b051034b0dc748bd399e3dab704645d4972
Daniel <svilladaniel@gmail.com>
2025-12-08 12:11:15 -0500

    despliegue continuo

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,10 +2,10 @@
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-devops-test-daniel"
-    key            = "terraform/state.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket        = "terraform-state-devops-test-daniel"
+    key           = "terraform/state.tfstate"
+    region        = "us-east-1"
+    use_lockfile  = true
+    encrypt       = true
   }
 }
